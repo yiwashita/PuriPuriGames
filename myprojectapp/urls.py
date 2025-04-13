eb7909fdf630a58d8dbb2f3c563db6a9672d66a9
yiwashita <y.iwashita0228@gmail.com>
2025-04-14 00:10:30 +0900

    サイトページの追加、unityゲームの取り込み

--- myprojectapp/urls.py
+++ myprojectapp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('top/', views.myprojectfunction),
+    path('', views.myprojectfunction), # トップページ
+    path('recipe/', views.recipe), # レシピページ
+    path('BrockDestroy/', views.BrockDestroy), # BrockDestroyページ
+
+    path('unity/', views.unity_index),
 ]
--- myprojectapp/views.py
+++ myprojectapp/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 #追加
 def myprojectfunction(request):
     return render(request, 'index.html')
+
+def recipe(request):
+    return render(request, 'recipe.html')
+
+def BrockDestroy(request):
+    return render(request, 'BrockDestroy.html')
+
+def unity_index(request):
+    return render(request, 'unity_index.html')
